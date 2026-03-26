9b2745590613ddb4e59737b1ba73a97494cb885b
Anja_vonMoos <Anja_vonMoos@sluz.ch>
2026-03-26 08:59:39 +0100

    Daten einfügen -> über seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Plant;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +18,23 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+        $plant = new Plant();
+        $plant->name = 'Tomate';
+        $plant->slug = 'tomate';
+        $plant->description = 'Tomaten sind Früchte der Tomatenpflanze.';
+        $plant->stock = 10;
+        $plant->save();
+
+        $plant = new Plant();
+        $plant->name = 'Kartoffel';
+        $plant->slug = 'kartoffel';
+        $plant->description = 'Die Kartoffel ist eine Pflanzenart aus der Gattung der Nachtschattengewächse (Solanaceae).';
+        $plant->stock = 20;
+        $plant->save();
     }
 }
